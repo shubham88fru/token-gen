ed517ccc697ffea25b493d5a69b8d404cb308e43
Shubham Singh <shubham88fru@yahoo.com>
2024-01-23 11:09:25 -0700

    add proper labels

--- src/components/InputCard.js
+++ src/components/InputCard.js
@@ -1,43 +1,43 @@
 import { useState } from "react";
 
 function InputCard({ handleSubmitClick }) {
-  const [value1, setValue1] = useState("");
-  const [value2, setValue2] = useState("");
-  const [value3, setValue3] = useState("");
+  const [app, setApp] = useState("");
+  const [appId, setAppId] = useState("");
+  const [api, setApi] = useState("");
 
   return (
     <div className="flex-item card input-card">
       <form className="input-form" onSubmit={handleSubmitClick}>
         <div>
-          <label htmlFor="input1">Input 1:</label>
+          <label htmlFor="input1">App:</label>
           <input
             id="input1"
-            placeholder="input1"
+            placeholder="App name.."
             required
-            value={value1}
-            onChange={(e) => setValue1(e.target.value)}
+            value={app}
+            onChange={(e) => setApp(e.target.value)}
           ></input>
         </div>
 
         <div>
-          <label htmlFor="input2">Input 2:</label>
+          <label htmlFor="input2">App Id: </label>
           <input
             id="input2"
-            placeholder="input2"
+            placeholder="App id.."
             required
-            value={value2}
-            onChange={(e) => setValue2(e.target.value)}
+            value={appId}
+            onChange={(e) => setAppId(e.target.value)}
           ></input>
         </div>
 
         <div>
-          <label htmlFor="input3">Input 3:</label>
+          <label htmlFor="input3">API:</label>
           <input
             id="input3"
-            placeholder="input3"
+            placeholder="Api.."
             required
-            value={value3}
-            onChange={(e) => setValue3(e.target.value)}
+            value={api}
+            onChange={(e) => setApi(e.target.value)}
           ></input>
         </div>
 
